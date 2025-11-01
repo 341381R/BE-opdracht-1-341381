d45b296ba351094d82a5c98b65053abcf3f1c083
unknown <341381@studet.mboutrecht.nl>
2025-11-01 20:45:54 +0100

    de call van getAllProducts in de model gemaakt

---
+++ app/Models/MagazijnModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class MagazijnModel extends Model
+{
+    public function SP_GetAllProducts()
+    {
+        // var_dump(DB::select('CALL SP_GetAllAllergenen'));
+        return DB::select('CALL SP_GetAllProducts');
+    }
+}
