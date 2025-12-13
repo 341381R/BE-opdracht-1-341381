ce9971058937be1e4ee4b59c52c7c7f9860eab3d
unknown <341381@studet.mboutrecht.nl>
2025-12-13 17:03:53 +0100

    stored procedure GetAllLeveranciers toegevoegd

---
+++ database/createscript/SPGetAllLeveranciers.sql
@@ -0,0 +1,22 @@
+DROP PROCEDURE IF EXISTS SP_GetAllLeveranciers;
+
+DELIMITER $$
+
+CREATE PROCEDURE SP_GetAllLeveranciers(
+
+)
+BEGIN
+
+    SELECT 	 LVRC.Id
+			,LVRC.Naam
+			,LVRC.LeverancierNummer
+            ,LVRC.Mobiel
+            ,COUNT(DISTINCT PPLC.LeverancierId)   AS VerschillendeProducten
+	FROM Leverancier AS LVRC    
+    INNER JOIN ProductPerLeverancier AS PPLC
+    ON LVRC.Id = PPLC.LeverancierId
+    ORDER BY LVRC.Barcode DESC;
+
+END$$
+
+DELIMITER ;
