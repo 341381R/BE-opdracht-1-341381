04357adc6aedf817a9f2b8c720d250e6d30a3967
unknown <341381@studet.mboutrecht.nl>
2026-01-25 22:14:42 +0100

    de updateleverancier functie in de model geschreven

--- app/Models/LeverancierModel.php
+++ app/Models/LeverancierModel.php
@@ -39,4 +39,23 @@ class LeverancierModel extends Model
             ['id' => $id]
         ); 
     }
+
+    public function SP_UpdateLeverancier($id, $naam, $contactpersoon, $leveranciernummer, $mobiel, $straat, $huisnummer, $postcode, $stad)
+    {
+        $row = DB::selectOne(
+            'CALL SP_UpdateLeverancier(:id, :naam, :contactpersoon, :leveranciernummer, :mobiel, :straat, :huisnummer, :postcode, :stad)',
+            [
+                'id' => $id,
+                'naam' => $naam,
+                'contactpersoon' => $contactpersoon,
+                'leverancier' => $leveranciernummer,
+                'mobiel' => $mobiel,
+                'straat' => $straat,
+                'huisnummer' => $huisnummer,
+                'postcode' => $postcode,
+                'stad' => $stad,
+            ]
+        );
+        return $row->affected ?? 0;
+    }
 }
