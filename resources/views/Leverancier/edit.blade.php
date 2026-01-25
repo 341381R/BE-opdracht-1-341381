0a6eab1f0882321130777d3ab14278923d8a3990
unknown <341381@studet.mboutrecht.nl>
2026-01-25 22:21:38 +0100

    een fout in de edit gefixt

--- resources/views/Leverancier/edit.blade.php
+++ resources/views/Leverancier/edit.blade.php
@@ -32,37 +32,37 @@
             <div class="mb-3">
                 <label for="InputContactPersoon" class="form-label">ContactPersoon</label>
                 <input name="ContactPersoon" type="text" class="form-control" id="InputContactPersoon" aria-describedby="contactPersoonHelp"
-                    value="{{ old('naam', $leverancier->ContactPersoon) }}">
+                    value="{{ old('ContactPersoon', $leverancier->ContactPersoon) }}">
             </div>
             <div class="mb-3">
                 <label for="InputLeverancierNummer" class="form-label">LeverancierNummer</label>
                 <input name="LeverancierNummer" type="text" class="form-control" id="InputLeverancierNummer" aria-describedby="LeverancierNummerHelp"
-                    value="{{ old('naam', $leverancier->LeverancierNummer) }}">
+                    value="{{ old('LeverancierNummer', $leverancier->LeverancierNummer) }}">
             </div>
             <div class="mb-3">
                 <label for="InputMobiel" class="form-label">Mobiel</label>
                 <input name="Mobiel" type="text" class="form-control" id="InputMobiel" aria-describedby="MobielHelp"
-                    value="{{ old('naam', $leverancier->Mobiel) }}">
+                    value="{{ old('Mobiel', $leverancier->Mobiel) }}">
             </div>
             <div class="mb-3">
                 <label for="InputStraatnaam" class="form-label">Straatnaam</label>
                 <input name="Straatnaam" type="text" class="form-control" id="InputStraatnaam" aria-describedby="StraatnaamHelp"
-                    value="{{ old('naam', $leverancier->Straat) }}">
+                    value="{{ old('Straatnaam', $leverancier->Straat) }}">
             </div>
             <div class="mb-3">
                 <label for="InputHuisnummer" class="form-label">Huisnummer</label>
                 <input name="Huisnummer" type="text" class="form-control" id="InputHuisnummer" aria-describedby="HuisnummerHelp"
-                    value="{{ old('naam', $leverancier->Huisnummer) }}">
+                    value="{{ old('Huisnummer', $leverancier->Huisnummer) }}">
             </div>
             <div class="mb-3">
                 <label for="InputPostcode" class="form-label">Postcode</label>
                 <input name="Postcode" type="text" class="form-control" id="InputPostcode" aria-describedby="PostcodeHelp"
-                    value="{{ old('naam', $leverancier->Postcode) }}">
+                    value="{{ old('Postcode', $leverancier->Postcode) }}">
             </div>
             <div class="mb-3">
                 <label for="InputStad" class="form-label">Stad</label>
                 <input name="Stad" type="text" class="form-control" id="InputStad" aria-describedby="StadHelp"
-                    value="{{ old('naam', $leverancier->Stad) }}">
+                    value="{{ old('Stad', $leverancier->Stad) }}">
             </div>
 
             <button type="submit" class="btn btn-primary">Opslaan</button>
