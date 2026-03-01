a2f28c6494363461ef83fa0f78cb4a03e4581f03
unknown <341381@studet.mboutrecht.nl>
2026-03-01 20:17:13 +0100

    Coalesce toegevoegd aan de stored procedure zodat wanneer iets null is het wordt vervangen met 0

--- database/createscript/SPSorteerAllergenen.sql
+++ database/createscript/SPSorteerAllergenen.sql
@@ -12,17 +12,17 @@ BEGIN
             ,PROD.Naam          AS ProductNaam
             ,ALGE.Naam          AS AllergeenNaam
             ,ALGE.Omschrijving
-            ,MAGA.AantalAanwezig
+            ,COALESCE(MAGA.AantalAanwezig, 0) AS AantalAanwezig
     FROM Allergeen AS ALGE
     INNER JOIN ProductPerAllergeen AS PPAN
     ON ALGE.Id = PPAN.AllergeenId
     INNER JOIN Product AS PROD
     ON PROD.Id = PPAN.ProductId
-    INNER JOIN Magazijn AS MAGA
+    LEFT JOIN Magazijn AS MAGA
     ON PROD.Id = MAGA.ProductId
-    INNER JOIN ProductPerLeverancier AS PPLC
+    LEFT JOIN ProductPerLeverancier AS PPLC
     ON PROD.Id = PPLC.ProductId
-    INNER JOIN Leverancier AS LVRC
+    LEFT JOIN Leverancier AS LVRC
     ON LVRC.Id = PPLC.LeverancierId
     WHERE p_categorie = ALGE.Naam
     ORDER BY ALGE.Naam;
