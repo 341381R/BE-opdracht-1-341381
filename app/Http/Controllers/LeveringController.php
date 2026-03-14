1c5f2b29b9df565b61093f1c426ef4a9e247d1a7
unknown <341381@studet.mboutrecht.nl>
2026-03-14 23:50:39 +0100

    de standaard controller en model gemaakt voor Levering

---
+++ app/Http/Controllers/LeveringController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LeverancierController;
+use Illuminate\Http\Request;
+
+class LeveringController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LeverancierController $leverancierController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LeverancierController $leverancierController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LeverancierController $leverancierController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LeverancierController $leverancierController)
+    {
+        //
+    }
+}
