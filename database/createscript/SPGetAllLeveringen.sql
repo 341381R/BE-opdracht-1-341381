07d7788ccff3c116e8300f82b9879f6f93cab94d
unknown <341381@studet.mboutrecht.nl>
2026-03-15 17:00:13 +0100

    een fout in de stored procedure gefixt

--- database/createscript/SPGetAllLeveringen.sql
+++ database/createscript/SPGetAllLeveringen.sql
@@ -20,8 +20,8 @@ BEGIN
     ON PPLC.ProductId = PROD.Id
     WHERE (p_StartDatum IS NULL OR PPLC.DatumLevering >= p_StartDatum) 
     AND (p_EindDatum IS NULL OR PPLC.DatumLevering <= p_EindDatum)
-    ORDER BY LVRC.Naam DESC
-    GROUP BY PROD.Naam;
+    GROUP BY PROD.Naam
+    ORDER BY LVRC.Naam DESC;
 
 END$$
 
