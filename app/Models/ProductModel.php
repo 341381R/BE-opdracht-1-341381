40fe340248c9ddd0f6b74fe9af329cd4c403b546
unknown <341381@studet.mboutrecht.nl>
2026-03-29 17:04:33 +0200

    de stored procedure functie toegevoegd aan de model

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductModel extends Model
 {
-    //
+    public function SP_GetAllProductsByDate($beginDatum, $eindDatum)
+    {
+        return DB::select(
+            'CALL SP_GetAllProductsByDate(:beginDatum, :eindDatum)',
+            [
+                'beginDatum' => $beginDatum,
+                'eindDatum' => $eindDatum
+            ]
+        );
+    }
 }
