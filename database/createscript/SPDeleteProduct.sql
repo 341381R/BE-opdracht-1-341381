894cc5c7581fa6cd0851faffaa59471b6707da5e
unknown <341381@studet.mboutrecht.nl>
2026-03-29 22:54:51 +0200

    de delete stored procedure aangemaakt voor product

---
+++ database/createscript/SPDeleteProduct.sql
@@ -0,0 +1,24 @@
+DROP PROCEDURE IF EXISTS SP_DeleteProduct;
+
+DELIMITER $$
+
+CREATE PROCEDURE SP_DeleteProduct(
+    IN p_id   INT
+)
+
+BEGIN
+
+    START TRANSACTION
+    DELETE FROM ProductPerAllergeen AS PPAN
+    WHERE p_id = PPAN.ProductId;
+    
+    DELETE FROM Product AS PROD
+    WHERE p_id = PROD.Id;
+
+    COMMIT;
+
+        SELECT ROW_COUNT() AS affected;
+
+END$$
+
+DELIMITER ;
