1ee9bc4461a1cccf624865d85fd050f0fa832fc1
unknown <341381@studet.mboutrecht.nl>
2026-03-29 22:56:52 +0200

    de model functie aangemaakt voor delete product

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -25,4 +25,14 @@ class ProductModel extends Model
             ['id' => $id]
         );
     }
+
+    public function SP_DeleteProduct($id)
+    {
+    
+        $result = DB::selectOne('CALL SP_DeleteProduct(:id)', [
+            'id' => $id
+        ]);
+
+        return $result->affected ?? null;
+    }
 }
