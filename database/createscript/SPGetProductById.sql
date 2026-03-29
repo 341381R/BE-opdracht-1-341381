c2ee5a81db07284ea5faab199cfdae226576a301
unknown <341381@studet.mboutrecht.nl>
2026-03-29 22:12:03 +0200

    de stored procedure gemaakt met de functie voor Product show

--- database/createscript/SPGetProductById.sql
+++ database/createscript/SPGetProductById.sql
@@ -9,25 +9,12 @@ BEGIN
 
     SELECT 	 PROD.Id
             ,PROD.Barcode
-			,CASE WHEN EXISTS (SELECT 1 FROM 
-            Allergeen AS ALGE
-            INNER JOIN ProductPerAllergeen AS PPAN 
-            ON ALGE.Id = PPAN.AllergeenId
-            WHERE PPAN.ProductId = p_id
-            AND ALGE.Id = 1
-            ) THEN "Ja" ELSE "Nee" END AS BevatGluten
-            ,LVRC.Mobiel
-            ,PROD.Naam                          AS ProductNaam
-            ,MAGA.AantalAanwezig
-            ,CONCAT(TRIM(TRAILING ".0" FROM MAGA.VerpakkingsEenheidInKilogram), " kg")	AS VerpakkingsEenheid
-            ,PPLC.DatumLevering
+			,CALL SP_CheckAllergeen(p_id, 1)
+			,CALL SP_CheckAllergeen(p_id, 2)
+			,CALL SP_CheckAllergeen(p_id, 3)
+			,CALL SP_CheckAllergeen(p_id, 4)
+			,CALL SP_CheckAllergeen(p_id, 5)
 	FROM Product AS PROD    
-    LEFT JOIN ProductPerLeverancier AS PPLC
-    ON LVRC.Id = PPLC.LeverancierId
-    LEFT JOIN Product AS PROD
-    ON PROD.Id = PPLC.ProductId
-    LEFT JOIN Magazijn AS MAGA
-    ON PROD.Id = MAGA.ProductId
     WHERE PROD.Id = p_id;
 
 END$$
