fa466de6bd45ce15e99b303b955f292038daf91a
unknown <341381@studet.mboutrecht.nl>
2026-03-29 16:56:28 +0200

    de stored procedure aangemaakt voor producten sorteren bij datum

---
+++ database/createscript/SPGetAllProductsByDate.sql
@@ -0,0 +1,31 @@
+DROP PROCEDURE IF EXISTS SP_GetAllProductsByDate;
+
+DELIMITER $$
+
+CREATE PROCEDURE SP_GetAllProductsByDate(
+    IN p_StartDatum DATE,
+    IN p_EindDatum DATE
+)
+BEGIN
+
+    SELECT 	 PROD.Id
+			,LVRC.Naam          AS LeverancierNaam
+            ,CNTC.Stad
+            ,PROD.Naam          AS ProductNaam
+            ,PELG.EinddatumLevering
+	FROM Product AS PROD    
+    INNER JOIN ProductPerLeverancier AS PPLC
+    ON PROD.Id = PPLC.ProductId
+    INNER JOIN Leverancier AS LVRC
+    ON PPLC.LeverancierId = LVRC.Id
+    INNER JOIN Contact AS CNTC
+    ON CNTC.Id = LVRC.ContactId
+    INNER JOIN ProductEinddatumLevering AS PELG
+    ON PROD.Id = ProductEinddatumLevering.ProductId
+    WHERE (p_StartDatum IS NULL OR PELG.EinddatumLevering >= p_StartDatum) 
+    AND (p_EindDatum IS NULL OR PELG.EinddatumLevering <= p_EindDatum)
+    ORDER BY PELG.EinddatumLevering DESC;
+
+END$$
+
+DELIMITER ;
