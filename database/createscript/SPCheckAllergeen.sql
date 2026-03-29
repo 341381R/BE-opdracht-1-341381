7f317202bc6841ae5ec557b63a4fc293b9d969ab
unknown <341381@studet.mboutrecht.nl>
2026-03-29 22:24:15 +0200

    de stored procedure veranderd in een functie voor het checken van allergenen

--- database/createscript/SPCheckAllergeen.sql
+++ database/createscript/SPCheckAllergeen.sql
@@ -1,19 +1,23 @@
-DROP PROCEDURE IF EXISTS SP_CheckAllergeen;
+DROP FUNCTION IF EXISTS FN_CheckAllergeen;
 
 DELIMITER $$
 
-CREATE PROCEDURE SP_CheckAllergeen (
-IN ProductId INT,
-IN AllergeenId INT
+CREATE FUNCTION FN_CheckAllergeen(
+	ProductId INT,
+	AllergeenId INT
 )
+RETURNS VARCHAR(3)
+DETERMINISTIC
 BEGIN
-	SELECT
+	DECLARE Resultaat VARCHAR(3);
+	SET Resultaat =
 		CASE WHEN EXISTS (SELECT 1 FROM 
             Allergeen AS ALGE
             INNER JOIN ProductPerAllergeen AS PPAN 
             ON ALGE.Id = PPAN.AllergeenId
             WHERE PPAN.ProductId = p_id
             AND ALGE.Id = AllergeenId
-            ) THEN "Ja" ELSE "Nee" END AS Resultaat;
+            ) THEN "Ja" ELSE "Nee" END;
+            return Resultaat;
     END $$
     DELIMITER ;
