61bd8ea7690ce28618e136ee56b01323dbce7da1
unknown <341381@studet.mboutrecht.nl>
2026-03-29 22:07:34 +0200

    een functie geschreven om show stored procedure minder lang te maken

---
+++ database/createscript/SPCheckAllergeen.sql
@@ -0,0 +1,19 @@
+DROP PROCEDURE IF EXISTS SP_CheckAllergeen;
+
+DELIMITER $$
+
+CREATE PROCEDURE SP_CheckAllergeen (
+IN ProductId INT,
+IN AllergeenId INT
+)
+BEGIN
+	SELECT
+		CASE WHEN EXISTS (SELECT 1 FROM 
+            Allergeen AS ALGE
+            INNER JOIN ProductPerAllergeen AS PPAN 
+            ON ALGE.Id = PPAN.AllergeenId
+            WHERE PPAN.ProductId = p_id
+            AND ALGE.Id = 1
+            ) THEN "Ja" ELSE "Nee" END AS Resultaat;
+    END $$
+    DELIMITER ;
